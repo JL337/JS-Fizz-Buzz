a9a202d69144bd95bab1c08849736362321e3693
Tech-A23 <tech-a23@tech-a23s-air.home>
2017-12-12 06:57:30 +0000

    Added console clear functionality and cleared line indenting

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -6,18 +6,21 @@ var loop = "y";
 
 while (loop === "y")
 {
-	choice = prompt("Do you want to calculate the FizzBuzz result after entering one interger number? y/n?");
+	choice = prompt("Do you want to calculate the FizzBuzz result after entering a single interger? y/n?");
 		if (choice === "y")
 		{
-			value1 = parseInt(prompt("Enter first interger number!"));
-				if (value1 % 15 === 0)
-					alert("FizzBuzz");
-				else if (value1 % 5 === 0)
-					alert("Buzz");
-				else if (value1 % 3 === 0)
-					alert("Fizz");
+			value1 = parseInt(prompt("Enter a single interger!"));
+				if (value1 % 15 == 0)
+				//if input is a multiple of 15
+					alert((value1)+" is a FizzBuzz");
+				else if (value1 % 5 == 0)
+				//if input is a multiple of 5
+					alert((value1)+" is Buzz");
+				else if (value1 % 3 == 0)
+					alert((value1)+" is Fizz");
+				//if input is a multiple of 3
 				else
-					alert("This is neither a Fizz nor Buzz!");		
+					alert((value1)+" is neither a Fizz nor Buzz!");		
 		}
 		else if (choice === "n")
 		{
@@ -25,17 +28,19 @@ while (loop === "y")
 			if (choice === "y")
 			{
 				value1 = parseInt(prompt("Enter the total number of intergers you would like to increment through!"));
-						for (var i=1; i <= value1; i++)
-						{
-								if (i % 15 === 0)
-							        console.log("FizzBuzz");
-							    else if (i % 5 === 0)
-							        console.log("Buzz");
-							    else if (i % 3 === 0)
-							        console.log("Fizz");
-							    else 
-							        console.log(i);
-						}	
+				console.clear();
+				//clears console if the console has been used before to calculate multiple Fizz
+				for (var i=1; i <= value1; i++)
+				{
+						if (i % 15 == 0)
+					        console.log((i)+" is a FizzBuzz");
+					    else if (i % 5 == 0)
+					        console.log((i)+" is Buzz");
+					    else if (i % 3 == 0)
+					        console.log((i)+" is Fizz");
+					    else
+					        console.log(i);
+				}	
 			} 	
 		} 
 	loop = prompt("Run again, y/n?");
